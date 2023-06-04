a2ccd0c493dd039169bce559af3ddfc2f8b1749b
williambl <william@williambl.com>
2023-06-04 14:49:17 +0100

    feat: add player movement

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 mod alloc;
 mod wasm4;
 use wasm4::*;
-use crate::GameState::{Game, Menu};
+use crate::State::{Game, Menu};
 
 #[rustfmt::skip]
 const SMILEY: [u8; 8] = [
@@ -16,7 +16,7 @@ const SMILEY: [u8; 8] = [
     0b11000011,
 ];
 
-static mut STATE: GameState = Menu(MenuState { selected: 0, pressed: false });
+static mut STATE: State = Menu(MenuState { selected: 0, pressed: false });
 static mut LAST_GAMEPAD: u8 = 0;
 
 #[no_mangle]
@@ -25,11 +25,11 @@ fn update() {
     let last_gamepad = unsafe { LAST_GAMEPAD };
     let state = match unsafe { STATE } {
         Menu(state) => update_menu(state, gamepad, last_gamepad),
-        Game => Game,
+        Game(state) => update_game(state, gamepad, last_gamepad),
     };
     match state {
         Menu(state) => render_menu(state),
-        Game => {},
+        Game(state) => render_game(state),
     }
 
     unsafe { STATE = state };
@@ -37,9 +37,9 @@ fn update() {
 }
 
 #[derive(Copy, Clone)]
-enum GameState {
+enum State {
     Menu(MenuState),
-    Game,
+    Game(GameState),
 }
 
 #[derive(Copy, Clone)]
@@ -48,7 +48,7 @@ struct MenuState {
     pressed: bool,
 }
 
-fn update_menu(state: MenuState, gamepad: u8, last_gamepad: u8) -> GameState {
+fn update_menu(state: MenuState, gamepad: u8, last_gamepad: u8) -> State {
     let mut new_state = state;
     if gamepad & !last_gamepad & BUTTON_UP != 0 {
         new_state.selected = new_state.selected.wrapping_sub(1) % 2;
@@ -60,7 +60,13 @@ fn update_menu(state: MenuState, gamepad: u8, last_gamepad: u8) -> GameState {
         new_state.pressed = true;
     } else if last_gamepad & BUTTON_1 != 0 {
         if new_state.selected == 0 {
-            return Game
+            return Game(GameState {
+                player_x: 0,
+                player_y: 0,
+                player_dx: 0,
+                player_dy: 0,
+                time: 0,
+            })
         } else {
             panic!()
         }
@@ -77,3 +83,64 @@ fn render_menu(state: MenuState) {
     unsafe { *DRAW_COLORS = if state.selected == 1 { if state.pressed { 0x0002 } else { 0x0004 } } else { 0x0003 } }
     text("Exit", 10, 40);
 }
+
+#[derive(Copy, Clone)]
+struct GameState {
+    player_x: u8,
+    player_y: u8,
+    player_dx: i8,
+    player_dy: i8,
+    time: u32,
+}
+
+fn update_movement_from_gamepad(gamepad: u8, state: GameState) -> GameState {
+    let mut new_state = state;
+    if gamepad & BUTTON_UP != 0 {
+        new_state.player_dy -= 1;
+    }
+    if gamepad & BUTTON_DOWN != 0 {
+        new_state.player_dy += 1;
+    }
+    if gamepad & BUTTON_LEFT != 0 {
+        new_state.player_dx -= 1;
+    }
+    if gamepad & BUTTON_RIGHT != 0 {
+        new_state.player_dx += 1;
+    }
+    new_state
+}
+
+fn update_movement(mut state: GameState, gamepad: u8, last_gamepad: u8) -> GameState {
+    state.player_dx = 0;
+    state.player_dy = 0;
+
+    state = update_movement_from_gamepad(gamepad, state);
+    state = update_movement_from_gamepad(last_gamepad, state);
+
+    state.player_x = if state.player_dx < 0 {
+        state.player_x.saturating_sub(state.player_dx.saturating_abs() as u8)
+    } else {
+        state.player_x.saturating_add(state.player_dx as u8)
+    }.clamp(0, 160);
+    state.player_y = if state.player_dy < 0 {
+        state.player_y.saturating_sub(state.player_dy.saturating_abs() as u8)
+    } else {
+        state.player_y.saturating_add(state.player_dy as u8)
+    }.clamp(0, 160);
+
+    state
+}
+
+fn update_game(state: GameState, gamepad: u8, last_gamepad: u8) -> State {
+    let mut new_state = state;
+    new_state.time += 1;
+    new_state = update_movement(new_state, gamepad, last_gamepad);
+
+    Game(new_state)
+}
+
+fn render_game(state: GameState) {
+    unsafe { *DRAW_COLORS = 0x0004 }
+    rect((state.player_x as i32 - 4), (state.player_y as i32 - 4), 8, 8);
+}
+
